5efb9c87887a4d8c7493da006632293b30eb99b4
AhmedZaman14 <zaman994ahmed@gmail.com>
2024-03-16 22:28:17 +0500

    q8 done: final commit

---
+++ Question8.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    "Ahmed Zaman",
+    "Abdul Moiz",
+    "Abdullah",
+    "Shahbaz",
+    "Ali Hassan",
+    "Tayyab"
+  ];
+
+  for (String name in names) {
+    print(name);
+  }
+}
